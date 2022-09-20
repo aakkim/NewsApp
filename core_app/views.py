73e2f8f486c99794cafb02c64cb65301afd6c41d
Angela Kim <aangelakkim@gmail.com>
2022-09-20 15:03:47 -0400

    finish project

--- core_app/views.py
+++ core_app/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 import requests
-API_KEY = 'fdf27bc7aebb4bd7a6d274af5bc2ab08'
+from dotenv import load_dotenv
+import os
+
+
+def configure():
+    load_dotenv()
 
 def home(request):
+    configure()
     country = request.GET.get('country')
     category = request.GET.get('category')
     
     if country:
-        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        url = f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={os.getenv('API_KEY')}"
         response = requests.get(url)
         data = response.json()
         articles = data['articles']
     else:
-        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        url = f"https://newsapi.org/v2/top-headlines?category={category}&apiKey={os.getenv('API_KEY')}"
         response = requests.get(url)
         data = response.json()
         articles = data['articles']
